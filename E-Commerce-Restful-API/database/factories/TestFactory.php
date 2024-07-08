1ab897f3279ba79a6d20d71d01031d8789134951
WuocThun <binngoc1963@gmail.com>
2024-07-08 16:17:17 +0700

    Seeding part for product and Reviews

--- E-Commerce-Restful-API/database/factories/TestFactory.php
+++ E-Commerce-Restful-API/database/factories/TestFactory.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Database\Factories\Model;
+namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ReviewFactory extends Factory
+class TestFactory extends Factory
 {
     /**
      * Define the model's default state.
--- E-Commerce-Restful-API/database/seeders/DatabaseSeeder.php
+++ E-Commerce-Restful-API/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      *
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //         \App\Models\User::factory(10)->create();
+        //        Review::factory('300')->create();
+        //        Product::factory('50')->create();
+        //        Review::factory()->count(300)->create();
+        Product::factory()->count(50)->create();
+        Review::factory()->count(300)->create();
+        //        factory(Product::class, 50)->create();
+        //        factory(Review::class, 300)->create();
     }
+
 }
